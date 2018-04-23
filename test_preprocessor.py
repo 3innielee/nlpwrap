12b67b3740ba4965c4c91df7becd482a41c09443
Winnie Lee <weiyi_lee@berkeley.edu>
2018-04-22 18:15:55 -0700

    add test- test lowercase

--- test_preprocessor.py
+++ test_preprocessor.py
@@ -3,3 +3,8 @@ import preprocessor
 
 def test_setup():
     assert(1 == 1)
+
+def test_transformTextCapitalShouldBeLowercase():
+    preprocessorObj=preprocessor()
+    transformed=preprocessorObj.transformText("HELLO")
+    assert(transformed=="hello")
