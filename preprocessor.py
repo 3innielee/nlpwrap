8afcd57ebc24e055f85a8ea4ee6b8fc92fb0ff03
Winnie Lee <weiyi_lee@berkeley.edu>
2018-04-22 18:14:47 -0700

    import libraries

--- preprocessor.py
+++ preprocessor.py
@@ -1,4 +1,12 @@
 # job: understands how to prepare raw text for further analysis
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
+from nltk.corpus import stopwords  
+from gensim import parsing
+import gensim
+import re
+
 class preprocessor():
     def __init__(self):
         return None
