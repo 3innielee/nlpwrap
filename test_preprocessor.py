e6160bc0471cff57c481f514ca493391a9c7251d
Winnie Lee <weiyi_lee@berkeley.edu>
2018-04-22 18:30:36 -0700

    fix import module

--- test_preprocessor.py
+++ test_preprocessor.py
@@ -1,5 +1,5 @@
 import pytest
-import preprocessor
+from preprocessor import preprocessor
 
 def test_setup():
     assert(1 == 1)
@@ -8,3 +8,8 @@ def test_transformTextCapitalShouldBeLowercase():
     preprocessorObj=preprocessor()
     transformed=preprocessorObj.transformText("HELLO")
     assert(transformed=="hello")
+
+def test_transformTextEmptyTextShouldBeEmpty():
+    preprocessorObj=preprocessor()
+    transformed=preprocessorObj.transformText("")
+    assert(transformed=="")
